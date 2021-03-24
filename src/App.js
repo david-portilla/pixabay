16b04f27d5918edb3edfbf73dd40e471a75de9ba
David Portilla <david_portilla@hotmail.com>
2021-03-24 11:39:45 -0500

    adding initial files, form and basic html

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Form from './Components/Form';
+
+function App () {
+  return (
+    <div className="container">
+      <div className="jumbotron">
+        <p className="lead text-center">Search for images</p>
+        <Form />
+      </div>
+    </div>
+  );
+}
+
+export default App;
