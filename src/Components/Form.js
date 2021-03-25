5340d557ef55f70cf7af3cf77b85776e20e55056
David Portilla <david_portilla@hotmail.com>
2021-03-25 12:17:38 -0500

    adding Form state and validate

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,14 +1,35 @@
 import React from 'react'
+import {useState} from 'react'
+import Error from './Error'
+
 
 const Form = () => {
+  const [term, saveTerm] = useState('')
+  const [error, setError] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    // validate form
+    if(term.trim() === '') {
+      setError(true)
+      return
+    }
+    setError(false)
+
+    // send term to api
+
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="row">
         <div className="form-group col-md-8">
           <input
             type="text"
             className="form-control form-control-lg"
             placeholder="Search for an image, ej: Soccer or mountain"
+            onChange={e => saveTerm(e.target.value)}
           />
         </div>
         <div className="form-group col-md-4">
@@ -19,6 +40,9 @@ const Form = () => {
           />
         </div>
       </div>
+
+      {error ? <Error message='Please submit a term for search' /> : null}
+
     </form>
   )
 }
