ae3a04cc142d25ceafb62bc324b5f820a06ef221
David Portilla <david_portilla@hotmail.com>
2021-03-30 10:03:01 -0500

    adding pagination and controls render components

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App () {
 
   const [searchTerm, saveResult] = useState('')
   const [images, setImages] = useState([])
-  const [currentPAge, setCurrentPAge] = useState(1)
-  const [totalPages, setTotalPages] = useState(1)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [totalPages, setTotalPages] = useState(5)
 
   useEffect(() => {
 
@@ -15,14 +15,28 @@ function App () {
       if(searchTerm === '') return
       const amountImage = 30
       const KEY = '20842004-1e614783a52a92aa8bd52a7d8'
-      const URL = `https://pixabay.com/api/?key=${ KEY }&q=${ searchTerm }&per_page=${ amountImage }`
+      const URL = `https://pixabay.com/api/?key=${ KEY }&q=${ searchTerm }&per_page=${ amountImage } &page=${ currentPage }`
       const request = await fetch(URL)
       const response = await request.json()
       setImages(response.hits)
       setTotalPages(Math.ceil(response.totalHits / amountImage))
+      const jumbotron = document.querySelector('.jumbotron')
+      jumbotron.scrollIntoView({behavior: 'smooth'})
     }
     requestApi()
-  }, [searchTerm])
+  }, [searchTerm, currentPage])
+
+  const previousPage = () => {
+    const newCurrentPage = currentPage - 1
+    if(newCurrentPage === 0) return
+    setCurrentPage(newCurrentPage)
+  }
+
+  const nextPage = () => {
+    const newCurrentPage = currentPage + 1
+    if(newCurrentPage > totalPages) return
+    setCurrentPage(newCurrentPage)
+  }
 
   return (
     <div className="container">
@@ -30,9 +44,23 @@ function App () {
         <p className="lead text-center">Search for images</p>
         <Form saveResult={saveResult} />
       </div>
-
       <div className="row justify-content-center">
         {images && <ListImages images={images}></ListImages>}
+        {(currentPage === 1) ? null : (
+          <button
+            className="bbtn btn-info mr-1 mb-5"
+            onClick={previousPage}
+          >
+            &laquo; Back
+          </button>
+        )}
+        {(currentPage === totalPages) ? null : (
+          <button
+            onClick={nextPage}
+            className="bbtn btn-info mb-5">
+            Next &raquo;
+          </button>
+        )}
       </div>
     </div>
   );
